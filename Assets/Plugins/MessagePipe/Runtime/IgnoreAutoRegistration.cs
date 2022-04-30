469e482fd607f7d085a9ea589e1d1c7190ba7e3f
rStar <starlove124760@gmail.com>
2022-05-01 04:57:59 +0800

    remove messagepipe from plugin

--- Assets/Plugins/MessagePipe/Runtime/IgnoreAutoRegistration.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace MessagePipe
-{
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
-    public class IgnoreAutoRegistration : Attribute
-    {
-    }
-}
--- Assets/Plugins/MessagePipe/Runtime/Internal/Preserve.cs
+++
@@ -1,8 +0,0 @@
-namespace MessagePipe.Internal
-{
-    // Preserve for Unity IL2CPP
-
-    internal class PreserveAttribute : System.Attribute
-    {
-    }
-}
