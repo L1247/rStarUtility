9ef85ff8e1a81adea8dd793f53d1043362054ca3
rStar <starlove124760@gmail.com>
2022-05-29 18:09:39 +0800

    fixed build error

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+#region Unity events
+
     // Start is called before the first frame update
     void Start()
     {
     #if UNITY_EDITOR
-    #endif
              EditorApplication.Beep();
+    #endif
     }
+
+#endregion
 }
