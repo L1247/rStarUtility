22b954d7d35429d526e63c466236a611233a2484
rStar <starlove124760@gmail.com>
2022-05-29 17:25:55 +0800

    add editor MonoBehaviour

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,11 @@
+using UnityEditor;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        EditorApplication.Beep();
+    }
+}
