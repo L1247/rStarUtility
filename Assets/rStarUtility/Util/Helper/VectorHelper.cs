eace7b9ea466a396587035676002fef0a8c5f9c9
rStar <starlove124760@gmail.com>
2022-06-12 16:40:56 +0800

    add summary

--- Assets/rStarUtility/Util/Helper/VectorHelper.cs
+++ Assets/rStarUtility/Util/Helper/VectorHelper.cs
@@ -10,15 +10,29 @@ namespace rStarUtility.Util.Helper
     {
     #region Public Variables
 
-        public static bool CloseThePointV2(Vector2 point1 , Vector2 point2 , float threshold)
+        /// <summary>
+        ///     判斷兩點接不接近，可調整magnitude數值去判斷兩者距離多近才算
+        /// </summary>
+        /// <param name="point1"></param>
+        /// <param name="point2"></param>
+        /// <param name="magnitude">向量長度</param>
+        /// <returns></returns>
+        public static bool CloseThePointV2(Vector2 point1 , Vector2 point2 , float magnitude)
         {
             var distance = (point1 - point2).sqrMagnitude;
-            return distance <= threshold;
+            return distance <= magnitude;
         }
 
-        public static bool CloseThePointV3(Vector3 point1 , Vector3 point2 , float threshold)
+        /// <summary>
+        ///     判斷兩點接不接近，可調整magnitude數值去判斷兩者距離多近才算
+        /// </summary>
+        /// <param name="point1"></param>
+        /// <param name="point2"></param>
+        /// <param name="magnitude">向量長度</param>
+        /// <returns></returns>
+        public static bool CloseThePointV3(Vector3 point1 , Vector3 point2 , float magnitude)
         {
-            return CloseThePointV2(point1 , point2 , threshold);
+            return CloseThePointV2(point1 , point2 , magnitude);
         }
 
     #endregion
