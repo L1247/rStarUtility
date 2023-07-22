cc1ff51cc1519edfeddfe77ef6b29e88fcaea314
r Star <starlove124760@gmail.com>
2023-07-22 21:16:59 +0800

    add ToggleActive and SetActive for renderer

--- Assets/rStarUtility/Util/Extensions/Unity/UnityExtension.cs
+++ Assets/rStarUtility/Util/Extensions/Unity/UnityExtension.cs
@@ -8,27 +8,7 @@ namespace rStarUtility.Util.Extensions.Unity
 {
     public static class UnityExtension
     {
-    #region Public Variables
-
-        public static bool ToggleActive(this MonoBehaviour monoBehaviour)
-        {
-            var gameObject   = monoBehaviour.gameObject;
-            var toggleActive = !gameObject.activeSelf;
-            gameObject.SetActive(toggleActive);
-            return toggleActive;
-        }
-
-        public static bool ToggleActive(this GameObject gameObject)
-        {
-            var toggleActive = !gameObject.activeSelf;
-            gameObject.SetActive(toggleActive);
-            return toggleActive;
-        }
-
-        public static void SetActive(this MonoBehaviour monoBehaviour , bool value)
-        {
-            monoBehaviour.gameObject.SetActive(value);
-        }
+    #region Public Methods
 
         public static bool IsActive(this MonoBehaviour monoBehaviour)
         {
@@ -40,9 +20,38 @@ namespace rStarUtility.Util.Extensions.Unity
             return transform.gameObject.activeSelf;
         }
 
+        public static void SetActive(this MonoBehaviour monoBehaviour , bool value)
+        {
+            monoBehaviour.gameObject.SetActive(value);
+        }
+
+        public static void SetActive(this Renderer renderer , bool value)
+        {
+            renderer.gameObject.SetActive(value);
+        }
+
         public static void SetActive(this Transform transform , bool value)
         {
             transform.gameObject.SetActive(value);
+        }
+
+        public static bool ToggleActive(this MonoBehaviour monoBehaviour)
+        {
+            var gameObject = monoBehaviour.gameObject;
+            return gameObject.ToggleActive();
+        }
+
+        public static bool ToggleActive(this Renderer renderer)
+        {
+            var gameObject = renderer.gameObject;
+            return gameObject.ToggleActive();
+        }
+
+        public static bool ToggleActive(this GameObject gameObject)
+        {
+            var toggleActive = !gameObject.activeSelf;
+            gameObject.SetActive(toggleActive);
+            return toggleActive;
         }
 
     #endregion
