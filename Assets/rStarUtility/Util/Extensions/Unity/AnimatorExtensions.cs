1af651ba74091877605e156bec917626ced54cad
r Star <starlove124760@gmail.com>
2023-08-10 04:26:15 +0800

    add AnimatorExtensions.cs

---
+++ Assets/rStarUtility/Util/Extensions/Unity/AnimatorExtensions.cs
@@ -0,0 +1,41 @@
+#region
+
+using UnityEngine;
+
+#endregion
+
+namespace rStarUtility.Util.Extensions.Unity
+{
+    public static class AnimatorExtensions
+    {
+    #region Public Methods
+
+        public static string GetCurrentClipName(this Animator animator)
+        {
+            var clipName = animator.GetCurrentAnimatorClipInfo(0)[0].clip.name;
+            return clipName;
+        }
+
+        public static bool IsAnimationPlayEnding(this Animator animator , string animationName)
+        {
+            var isPlayingAnimation    = animator.IsPlayingAnimation(animationName);
+            var isPlayEnding          = animator.IsPlayEnding();
+            var isAnimationPlayEnding = isPlayingAnimation && isPlayEnding;
+            return isAnimationPlayEnding;
+        }
+
+        public static bool IsPlayEnding(this Animator animator)
+        {
+            var animationPlayEnding = animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1;
+            return animationPlayEnding;
+        }
+
+        public static bool IsPlayingAnimation(this Animator animator , string animationName)
+        {
+            var isPlayingAnimation = GetCurrentClipName(animator) == animationName;
+            return isPlayingAnimation;
+        }
+
+    #endregion
+    }
+}
