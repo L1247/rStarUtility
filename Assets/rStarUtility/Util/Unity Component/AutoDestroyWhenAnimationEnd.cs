079a88a0e26ec726a4569a1ab3d69889815cc137
r Star <starlove124760@gmail.com>
2023-11-08 02:17:38 +0800

    add AutoDestroyWhenAnimationEnd

---
+++ Assets/rStarUtility/Util/Unity Component/AutoDestroyWhenAnimationEnd.cs
@@ -0,0 +1,41 @@
+#region
+
+using rStarUtility.Util.Extensions.Csharp;
+using rStarUtility.Util.Extensions.Unity;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+#endregion
+
+namespace rStarUtility.Util.Component
+{
+    public class AutoDestroyWhenAnimationEnd : MonoBehaviour
+    {
+    #region Private Variables
+
+        [SerializeField]
+        private string animationName;
+
+        [SerializeField]
+        private Animator animator;
+
+    #endregion
+
+    #region Unity events
+
+        private void Awake()
+        {
+            animator ??= GetComponent<Animator>();
+            Assert.IsNotNull(animator , "Invalid case , cause the animator is null or empty.");
+            Assert.IsTrue(animationName.HasValue() , "animationName string is null or empty.");
+        }
+
+        private void Update()
+        {
+            if (animator.IsAnimationPlayEnding(animationName).IsFalse()) return;
+            Destroy(gameObject);
+        }
+
+    #endregion
+    }
+}
