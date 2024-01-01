599ac0827021988dc004a566fccf53b0a6490cde
r Star <starlove124760@gmail.com>
2024-01-01 16:54:51 +0800

    add set extensions for vector

--- Assets/rStarUtility/Util/Extensions/Unity/VectorExtensions.cs
+++ Assets/rStarUtility/Util/Extensions/Unity/VectorExtensions.cs
@@ -22,6 +22,36 @@ namespace rStarUtility.Util.Extensions.Unity
             return vector2;
         }
 
+        public static Vector2 SetX(this Vector2 vector2 , float x)
+        {
+            vector2.x = x;
+            return vector2;
+        }
+
+        public static Vector3 SetX(this Vector3 vector3 , float x)
+        {
+            vector3.x = x;
+            return vector3;
+        }
+
+        public static Vector2 SetY(this Vector2 vector2 , float y)
+        {
+            vector2.y = y;
+            return vector2;
+        }
+
+        public static Vector3 SetY(this Vector3 vector3 , float y)
+        {
+            vector3.y = y;
+            return vector3;
+        }
+
+        public static Vector3 SetZ(this Vector3 vector3 , float z)
+        {
+            vector3.z = z;
+            return vector3;
+        }
+
     #endregion
     }
 }
