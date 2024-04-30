49b7f9f7a11702e2c714e8c49112644b6da63093
r Star <starlove124760@gmail.com>
2024-05-01 04:15:51 +0800

    add NullInput

--- Assets/rStarUtility/Util/DDD/UseCase/Input.cs
+++ Assets/rStarUtility/Util/DDD/UseCase/Input.cs
@@ -1,4 +1,20 @@
 namespace rStarUtility.Util.DDD.UseCase
 {
-    public interface Input { }
+    public interface Input
+    {
+    #region Public Methods
+
+        static NullInput OfNull()
+        {
+            return new NullInput();
+        }
+
+    #endregion
+
+    #region Nested Types
+
+        class NullInput : Input { }
+
+    #endregion
+    }
 }
